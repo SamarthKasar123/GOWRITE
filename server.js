10f6a3e70784bb503a62d658f2333cc651b2d40d
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-01-08 20:01:43 +0530

    Added Google OAuth Method for the Login and Register

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import dotenv from 'dotenv';
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+
 
 dotenv.config();
 
@@ -74,6 +76,37 @@ passport.use(new Strategy({
         return cb(err);
     }
 }));
+
+passport.use(
+    "google",
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          console.log(profile);
+          const result = await db.query("SELECT * FROM users WHERE email = $1", [
+            profile.email,
+          ]);
+          if (result.rows.length === 0) {
+            const newUser = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2)",
+              [profile.email, "google"]
+            );
+            return cb(null, newUser.rows[0]);
+          } else {
+            return cb(null, result.rows[0]);
+          }
+        } catch (err) {
+          return cb(err);
+        }
+      }
+    )
+  );
 
 passport.serializeUser((user, cb) => {
     cb(null, user.id);
@@ -183,6 +216,30 @@ app.post("/register", async (req, res) => {
         });
     }
 });
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+  
+  app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", {
+      successRedirect: "/secrets",
+      failureRedirect: "/login",
+    })
+  );
+
+  app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
 
 app.post("/login", (req, res, next) => {
     passport.authenticate("local", (err, user, info) => {
