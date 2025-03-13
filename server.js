57ac2272fe901e9f69b54a2da80536f7f47bf6a2
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-03-13 23:11:57 +0530

    Small changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 
 
-dotenv.config();
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
